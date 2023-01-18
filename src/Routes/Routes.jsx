8438d641deb2dcc16cae2e4898708b2fad916038
Agustín García <agustin.tavoite@gmail.com>
2023-01-18 14:42:09 +0100

    .

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,6 +1,5 @@
 import { useSelector } from "react-redux";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { AuthProvider } from "../hooks/useFirebase";
 import SignInSide from "../components/Login/Login";
 import Logout from "../components/Logout.jsx";
 import ForgotPassword from "../components/Login/ForgotPassword.jsx";
@@ -34,208 +33,206 @@ export default function RouterApp() {
   return (
     <div>
       <Router>
-        <AuthProvider>
-          <div>
-            <Routes>
-              <Route path="/" element={<SignInSide />} />
+        <div>
+          <Routes>
+            <Route path="/" element={<SignInSide />} />
 
-              <Route
-                path="/home"
-                element={
-                  <div>
-                    <Home />
-                  </div>
-                }
-              />
-              <Route
-                path="/favorites"
-                element={
-                  <>
-                    <SideMenu />
-                    <Favorites />
-                  </>
-                }
-              />
-              <Route
-                path="/acoustic"
-                element={
-                  <>
-                    <SideMenu />
-                    <Acoustic />
-                  </>
-                }
-              />
-              <Route
-                path="/alternative"
-                element={
-                  <>
-                    <SideMenu />
-                    <Alternative />
-                  </>
-                }
-              />
-              <Route
-                path="/reggaeton"
-                element={
-                  <>
-                    <SideMenu />
-                    <Reggaeton />
-                  </>
-                }
-              />
-              <Route
-                path="/latina"
-                element={
-                  <>
-                    <SideMenu />
-                    <Latina />
-                  </>
-                }
-              />
-              <Route
-                path="/pop"
-                element={
-                  <>
-                    <SideMenu />
-                    <Pop />
-                  </>
-                }
-              />
-              <Route
-                path="/rock"
-                element={
-                  <>
-                    <SideMenu />
-                    <Rock />
-                  </>
-                }
-              />
-              <Route
-                path="/classic"
-                element={
-                  <>
-                    <SideMenu />
-                    <Classic />
-                  </>
-                }
-              />
-              <Route
-                path="/techno"
-                element={
-                  <>
-                    <SideMenu />
-                    <Techno />
-                  </>
-                }
-              />
-              <Route
-                path="/rap"
-                element={
-                  <>
-                    <SideMenu />
-                    <Rap />
-                  </>
-                }
-              />
-              <Route
-                path="/profile"
-                element={
-                  <>
-                    <SideMenu />
-                    <Profile />
-                  </>
-                }
-              />
-              <Route path="/admin" element={<AdminView />} />
-              <Route
-                path="/user/resetpassword/:token"
-                element={<ResetPassword />}
-              />
-              <Route path="/forgot" element={<ForgotPassword />} />
-              <Route
-                path="/signup"
-                element={
-                  <>
-                    <Signup />
-                  </>
-                }
-              />
-              <Route
-                path="/logout"
-                element={
-                  <>
-                    <SideMenu />
-                    <Logout />
-                  </>
-                }
-              />
-              <Route
-                path="/edit"
-                element={
-                  <>
-                    <SideMenu />
-                    <EditUser />
-                  </>
-                }
-              />
+            <Route
+              path="/home"
+              element={
+                <div>
+                  <Home />
+                </div>
+              }
+            />
+            <Route
+              path="/favorites"
+              element={
+                <>
+                  <SideMenu />
+                  <Favorites />
+                </>
+              }
+            />
+            <Route
+              path="/acoustic"
+              element={
+                <>
+                  <SideMenu />
+                  <Acoustic />
+                </>
+              }
+            />
+            <Route
+              path="/alternative"
+              element={
+                <>
+                  <SideMenu />
+                  <Alternative />
+                </>
+              }
+            />
+            <Route
+              path="/reggaeton"
+              element={
+                <>
+                  <SideMenu />
+                  <Reggaeton />
+                </>
+              }
+            />
+            <Route
+              path="/latina"
+              element={
+                <>
+                  <SideMenu />
+                  <Latina />
+                </>
+              }
+            />
+            <Route
+              path="/pop"
+              element={
+                <>
+                  <SideMenu />
+                  <Pop />
+                </>
+              }
+            />
+            <Route
+              path="/rock"
+              element={
+                <>
+                  <SideMenu />
+                  <Rock />
+                </>
+              }
+            />
+            <Route
+              path="/classic"
+              element={
+                <>
+                  <SideMenu />
+                  <Classic />
+                </>
+              }
+            />
+            <Route
+              path="/techno"
+              element={
+                <>
+                  <SideMenu />
+                  <Techno />
+                </>
+              }
+            />
+            <Route
+              path="/rap"
+              element={
+                <>
+                  <SideMenu />
+                  <Rap />
+                </>
+              }
+            />
+            <Route
+              path="/profile"
+              element={
+                <>
+                  <SideMenu />
+                  <Profile />
+                </>
+              }
+            />
+            <Route path="/admin" element={<AdminView />} />
+            <Route
+              path="/user/resetpassword/:token"
+              element={<ResetPassword />}
+            />
+            <Route path="/forgot" element={<ForgotPassword />} />
+            <Route
+              path="/signup"
+              element={
+                <>
+                  <Signup />
+                </>
+              }
+            />
+            <Route
+              path="/logout"
+              element={
+                <>
+                  <SideMenu />
+                  <Logout />
+                </>
+              }
+            />
+            <Route
+              path="/edit"
+              element={
+                <>
+                  <SideMenu />
+                  <EditUser />
+                </>
+              }
+            />
 
-              <Route
-                path="/songs"
-                element={
-                  <>
-                    <SideMenu />
-                    <Songs />
-                  </>
-                }
-              />
-              <Route
-                path="/playlists"
-                element={
-                  <>
-                    <SideMenu />
-                    <Playlists />
-                  </>
-                }
-              />
-              <Route
-                path="/playlist-edit"
-                element={
-                  <>
-                    <SideMenu />
-                    <PlaylistEdit />
-                  </>
-                }
-              />
-              <Route
-                path="/search"
-                element={
-                  <>
-                    <SideMenu />
-                    <Search />
-                  </>
-                }
-              />
-              <Route
-                path="/playlist/public/:id"
-                element={
-                  <>
-                    <SideMenu />
-                    <PublicPlaylist />
-                  </>
-                }
-              />
-              <Route
-                path="/playlist/:id"
-                element={
-                  <>
-                    <SideMenu />
-                    <UserPlaylist />
-                  </>
-                }
-              />
-            </Routes>
-          </div>
-        </AuthProvider>
+            <Route
+              path="/songs"
+              element={
+                <>
+                  <SideMenu />
+                  <Songs />
+                </>
+              }
+            />
+            <Route
+              path="/playlists"
+              element={
+                <>
+                  <SideMenu />
+                  <Playlists />
+                </>
+              }
+            />
+            <Route
+              path="/playlist-edit"
+              element={
+                <>
+                  <SideMenu />
+                  <PlaylistEdit />
+                </>
+              }
+            />
+            <Route
+              path="/search"
+              element={
+                <>
+                  <SideMenu />
+                  <Search />
+                </>
+              }
+            />
+            <Route
+              path="/playlist/public/:id"
+              element={
+                <>
+                  <SideMenu />
+                  <PublicPlaylist />
+                </>
+              }
+            />
+            <Route
+              path="/playlist/:id"
+              element={
+                <>
+                  <SideMenu />
+                  <UserPlaylist />
+                </>
+              }
+            />
+          </Routes>
+        </div>
       </Router>
       {activeSong?.title && (
         <div className="fixed h-28 bottom-0 left-0 right-0 flex animate-slideup bg-white rounded-t-3xl z-50">
--- src/components/Logout.jsx
+++ src/components/Logout.jsx
@@ -1,30 +1,30 @@
-import { Button } from '@mui/material'
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { useAuth } from '../hooks/useFirebase';
+// import { Button } from '@mui/material'
+// import { useState } from 'react';
+// import { Link } from 'react-router-dom';
+// import { useAuth } from '../hooks/useFirebase';
 
-export default function Logout() { 
+// export default function Logout() { 
 
-  const [datos] = useState({
-    email: "",
-   });
+//   const [datos] = useState({
+//     email: "",
+//    });
 
-const {logout } = useAuth();
+// const {logout } = useAuth();
     
    
-    const handleLogout = async () => {
-      try {
-        await logout();
-        localStorage.clear();
-      } catch (error) {
-        console.error(error.message);
-      }  }
-  return (
-    <>
-    <h3> WELCOME {datos.email}</h3>
+//     const handleLogout = async () => {
+//       try {
+//         await logout();
+//         localStorage.clear();
+//       } catch (error) {
+//         console.error(error.message);
+//       }  }
+//   return (
+//     <>
+//     <h3> WELCOME {datos.email}</h3>
 
-    <Link to="/"><Button onClick={handleLogout}>{logout ? "Sign Out" : "Login"}</Button>
-</Link> </> 
- )
-}
+//     <Link to="/"><Button onClick={handleLogout}>{logout ? "Sign Out" : "Login"}</Button>
+// </Link> </> 
+//  )
+// }
 
